d89b1fb55c35d5297cdb5c4968609018113acbda
Yusaf Osei-Bonsu <46824238+YusafOsei-Bonsu@users.noreply.github.com>
2020-09-28 12:44:25 +0100

    Displaying each service as a list item

--- app/routes/services.js
+++ app/routes/services.js
@@ -1,4 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default class ServicesRoute extends Route {
+    model() {
+        return ['PC Repair', 'Network Diagnostics', 'Home Audio Installations'];
+    }
 }
