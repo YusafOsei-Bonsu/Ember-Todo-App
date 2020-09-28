493e29a3564b4416979087cf1942d969e36825aa
Yusaf Osei-Bonsu <46824238+YusafOsei-Bonsu@users.noreply.github.com>
2020-09-28 15:16:03 +0100

    Controller displays info about blog post in template

--- app/controllers/post.js
+++ app/controllers/post.js
@@ -1,4 +1,17 @@
 import Controller from '@ember/controller';
 
 export default class PostController extends Controller {
+    get title() {
+        return this.model.title;
+    }
+    get body() {
+        return this.model.body;
+    }
+    get authors() {
+        return this.model.authors;
+    }
+    get comments() {
+        return this.model.comments;
+    }
+    
 }
--- app/routes/post.js
+++ app/routes/post.js
@@ -1,4 +1,25 @@
 import Route from '@ember/routing/route';
 
 export default class PostRoute extends Route {
+    model() {
+        return {
+            title: "My blog post",
+            body: "This is the body of my blog post",
+            authors: ["William", "Robert", "Michelle"],
+            comments: [
+                {
+                    name: "Mike Smith",
+                    comment: "Thanks for the great post"
+                },
+                {
+                    name: "John Doe",
+                    comment: "Nice post!"
+                },
+                {
+                    name: "Jeff Williams",
+                    comment: "Good job!"
+                },
+            ]
+        }
+    }
 }
