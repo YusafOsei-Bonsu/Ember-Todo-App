49e6927402c22686a35881ae18d2801639351a4c
Yusaf Osei-Bonsu <46824238+YusafOsei-Bonsu@users.noreply.github.com>
2020-09-29 15:08:00 +0100

    Path for the edit route

--- app/router.js
+++ app/router.js
@@ -18,7 +18,7 @@ Router.map(function() {
   this.route('post');
   this.route('cars', function() {
     this.route('new');
-    this.route('edit');
+    this.route('edit', {path: "/edit/:car_id"});
   });
   this.route('users');
 });
