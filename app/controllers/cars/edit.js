89fa7c7c221311042320b5a0edba3aa393205c83
Yusaf Osei-Bonsu <46824238+YusafOsei-Bonsu@users.noreply.github.com>
2020-09-29 15:46:54 +0100

    Can now change car details

--- app/controllers/cars/edit.js
+++ app/controllers/cars/edit.js
@@ -1,4 +1,24 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
 
 export default class CarsEditController extends Controller {
+    @action
+    editCar(id) {
+        const self = this;
+        const make = this.get('model.make');
+        const model = this.get('model.model');
+        const year = this.get('model.year');
+
+        this.store.findRecord('car', id)
+        .then((car) => {
+            car.set('make', make);
+            car.set('model', model);
+            car.set('year', year);
+            // car.save();
+            self.transitionToRoute('cars');
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+    }
 }
