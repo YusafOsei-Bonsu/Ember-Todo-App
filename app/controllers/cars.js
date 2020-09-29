08254634927c7f682f4b01e8ba2c014bbab17453
Yusaf Osei-Bonsu <46824238+YusafOsei-Bonsu@users.noreply.github.com>
2020-09-29 16:03:22 +0100

    Can delete car upon button press

--- app/controllers/cars.js
+++ app/controllers/cars.js
@@ -1,4 +1,11 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
 
 export default class CarsController extends Controller {
+    @action
+    deleteCar(id) {
+        // Deletes a certain car
+        let car = this.store.peekRecord('car', id);
+        car.destroyRecord();
+    }
 }
