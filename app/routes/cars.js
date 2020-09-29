425afd43c62970eb7410f0d6a65d08ac78a2cedc
Yusaf Osei-Bonsu <46824238+YusafOsei-Bonsu@users.noreply.github.com>
2020-09-29 12:36:09 +0100

    Commented out axios code

--- app/routes/cars.js
+++ app/routes/cars.js
@@ -1,10 +1,16 @@
 import Route from '@ember/routing/route';
-import axios from 'axios';
+// import axios from 'axios';
 
 export default class CarsRoute extends Route {
+    model() {
+        return this.store.findAll('car');
+    }
+
+    /* 
     async model() {
-        const response = await axios.get('/cars.json');
-        const cars = response.data;
+        // const response = await axios.get('/cars.json');
+        // const cars = response.data;
         return cars;
     }
+    */
 }
