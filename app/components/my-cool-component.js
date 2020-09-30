882fa068f1258c1ec8494e712a8e6cb6c243220b
Yusaf Osei-Bonsu <46824238+YusafOsei-Bonsu@users.noreply.github.com>
2020-09-30 10:52:20 +0100

    Pressing the button will greet the user; the greeting includes the user's name

--- app/components/my-cool-component.js
+++ app/components/my-cool-component.js
@@ -1,9 +1,14 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class MyCoolComponentComponent extends Component {
+    @tracked name = "Yusaf";
+    @tracked greetingSet = false;
+
     @action
-    greetUser() {
-        alert("Hello");
+    greetUser(name) {
+        alert(`Hello ${name}`);
+        this.greetingSet = true;
     }
 }
