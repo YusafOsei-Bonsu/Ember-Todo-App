eee50c32d5325a8eb6151a765958a5de10ccd63a
Yusaf Osei-Bonsu <46824238+YusafOsei-Bonsu@users.noreply.github.com>
2020-09-30 12:29:23 +0100

    Route to page that displays all GitHub repos

--- app/routes/github.js
+++ app/routes/github.js
@@ -1,4 +1,12 @@
 import Route from '@ember/routing/route';
+import axios from 'axios';
 
 export default class GithubRoute extends Route {
+    async model() {
+        const url = "https://api.github.com/users/YusafOsei-Bonsu/repos?sort=created_at&order=desc";
+        const response = await axios.get(url);
+        const repos = response.data.splice(0, 5);
+        console.log(repos);
+        return repos;
+    }
 }
