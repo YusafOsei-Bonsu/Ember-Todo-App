feaa3f79bc3c7f8a54e44e033cc1db255117c040
Yusaf Osei-Bonsu <46824238+YusafOsei-Bonsu@users.noreply.github.com>
2020-09-30 10:33:01 +0100

    Rendering a button ONCE which displays an alert box saying 'Hello' upon button press

--- app/components/my-cool-component.js
+++ app/components/my-cool-component.js
@@ -1,4 +1,9 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
 export default class MyCoolComponentComponent extends Component {
+    @action
+    greetUser() {
+        alert("Hello");
+    }
 }
